a73764df01b11dae76a7796394dfb0887293823c
lavaxcii <lavaxcii@gmail.com>
2022-07-14 11:31:41 +0200

    Complete all tests except last two; Write functions for rist two tests

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-function sum(a, b) {
+function capitalize(string) {
+  let splitString = string.split('');
+  splitString[0] = splitString[0].toUpperCase();
+  let result = splitString.toString().replace(/,/g, ""); 
+  return result;
+}
+
+function reverseString(string) {
+  let splitString = string.split('');
+  let result = [];
+  for (let i = splitString.length - 1; i >= 0; i--) {
+    result.push(splitString[i]);
+  };
+  result = result.toString().replace(/,/g, "");
+  return result;
+}
+
+function calculator(a, b) {
   return a + b;
 }
-module.exports = sum;
+
+function caesarCipher(a, b) {
+  return a + b;
+}
+
+function analyzeArray(a, b) {
+  return a + b;
+}
+
+
+module.exports = { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- testingMain.test.js
+++ testingMain.test.js
@@ -1,5 +1,31 @@
-const sum = require('./main');
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from './main';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+
+test('return capitalized string', () => {
+  expect(capitalize('kingBay')).toBe('KingBay');
+});
+
+test('return reversed string', () => {
+  expect(reverseString('kingBay')).toBe('yaBgnik');
+});
+
+test.skip('add 2 + 5', () => {
+  expect(calculator(2, 5)).toBe(7);
+});
+test.skip('subtract 5 - 2', () => {
+  expect(calculator(5, 2)).toBe(3);
+});
+test.skip('divide 6 / 3', () => {
+  expect(calculator(6, 3)).toBe(2);
+});
+test.skip('multiply 2 + 5', () => {
+  expect(calculator(2, 5)).toBe(10);
+});
+
+test.skip('stg', () => {
+  expect(caesarCipher('stg')).toBe('stgButIncipher');
+});
+
+test.skip('some arr with number', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toBe({ average: 4, min: 1, max: 8, length: 6 });
 });
